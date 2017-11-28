fa6165cce75befc64e53d160a39debf07255638a
johnmboudreaux <jhnbdrx@gmail.com>
2017-11-28 12:48:14 -0700

    generating random numbers and putting them together to get hex

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,13 @@
 
+const generateColors = () => {
+  const red = generateColorNumber();
+  const green = generateColorNumber();
+  const blue = generateColorNumber();
+
+  return `#${red}${green}${blue}`;
+};
+
+const generateColorNumber = () => {
+  const value = Math.floor(Math.random() * 255).toString(16);
+  return value;
+};
