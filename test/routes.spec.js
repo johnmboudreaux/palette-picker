1ffd682a2bc406315763f6c54b17bc1cbac4060c
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-01 11:04:16 -0700

    added testing

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -70,4 +70,62 @@ describe('API Routes', () => {
         });
     });
   });
+
+  describe('GET /api/v1/projects/:id', () => {
+    it('should return a specific project', (done) => {
+      chai.request(server)
+        .get('/api/v1/projects/1')
+        .then((response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(1);
+          response.body[0].should.have.property('id');
+          response.body[0].id.should.equal(1);
+          response.body[0].should.have.property('title');
+          response.body[0].title.should.equal('Star Wars');
+          response.body[0].should.have.property('created_at');
+          response.body[0].should.have.property('updated_at');
+          done();
+        })
+        .catch((error) => {
+          throw error;
+        });
+    });
+  });
+
+  describe('GET /api/v1/projects/:id/palettes', () => {
+    it('should return all palettes for a specific project', (done) => {
+      chai.request(server)
+        .get('/api/v1/projects/1/palettes')
+        .then((response) => {
+          response.should.have.status(201);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(2);
+          response.body[1].should.have.property('id');
+          response.body[1].id.should.equal(2);
+          response.body[1].should.have.property('name');
+          response.body[1].name.should.equal('palette2');
+          response.body[1].should.have.property('color1');
+          response.body[1].color1.should.equal('#ae0000');
+          response.body[1].should.have.property('color2');
+          response.body[1].color2.should.equal('#007ce6');
+          response.body[1].should.have.property('color3');
+          response.body[1].color3.should.equal('#2de3a2');
+          response.body[1].should.have.property('color4');
+          response.body[1].color4.should.equal('#b7a17c');
+          response.body[1].should.have.property('color5');
+          response.body[1].color5.should.equal('#c0b49d');
+          response.body[1].should.have.property('projectId');
+          response.body[1].projectId.should.equal(1);
+          response.body[1].should.have.property('created_at');
+          response.body[1].should.have.property('updated_at');
+          done();
+        })
+        .catch((error) => {
+          throw error;
+        });
+    });
+  });
 });
